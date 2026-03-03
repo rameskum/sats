43197e4e04a35c9ac76c1773c47ecb35dd89b5f6
Ramesh Kumar <rameskum.ms@outlook.com>
2026-03-03 09:49:55 -0500

    initial setup

---
+++ src/main/java/com/tds/tdvip/pde/vpda/sats/SatsApplication.java
@@ -0,0 +1,13 @@
+package com.tds.tdvip.pde.vpda.sats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tds/tdvip/pde/vpda/sats/SatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.tdvip.pde.vpda.sats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
